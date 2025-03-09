a8c7aa51fb37b60810d6cbff4ba708a8768c1927
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-03-09 23:52:13 +0200

    Create page.tsx

---
+++ my-app/app/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
+import AppointmentCart from "./_components/AppointmentCart";
+import Image from "next/image";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import CreateEvent from "./_components/CreateEvent";
+import { EventT, PickCalendarEvent, SelectDateEvent } from "./type";
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = () => {
+  const [events, setEvents] = useState<PickCalendarEvent[]>([]);
+
+  const [showEventForm, setShowEventForm] = useState(false);
+  const [datePayload, setDatePayload] = useState<SelectDateEvent | null>(null);
+  function handleSelectTime(payload: SelectDateEvent) {
+    setShowEventForm(true);
+    console.log(payload);
+    setDatePayload(payload);
+  }
+
+  return (
+    <div>
+      <Calendar
+        onSelectSlot={handleSelectTime}
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        selectable
+      />
+      {showEventForm && datePayload && (
+        <CreateEvent date={datePayload.start} endDate={datePayload.end} />
+      )}
+    </div>
+  );
+};
+export default MyCalendar;

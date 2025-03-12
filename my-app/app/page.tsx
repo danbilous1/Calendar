6996a46fc762fa415fffaab192da503420731a3d
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-03-12 22:06:11 +0200

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,16 +2,18 @@
 import { useEffect, useState } from "react";
 import AppointmentCart from "./_components/AppointmentCart";
 import Image from "next/image";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, SlotInfo } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import CreateEvent from "./_components/CreateEvent";
 import { EventT, PickCalendarEvent, SelectDateEvent } from "./type";
+import { useRouter } from "next/navigation";
 
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = () => {
   const [events, setEvents] = useState<PickCalendarEvent[]>([]);
+  const router = useRouter();
   useEffect(() => {
     fetch("/api/event")
       .then((res) => res.json())
@@ -30,16 +32,22 @@ const MyCalendar = () => {
   const [datePayload, setDatePayload] = useState<SelectDateEvent | null>(null);
   function handleSelectTime(payload: SelectDateEvent) {
     setShowEventForm(true);
-    console.log(payload);
+    console.log(1, payload);
     setDatePayload(payload);
+  }
+  function handleSelectEvent(payload: SlotInfo) {
+    console.log(2, payload);
+    setShowEventForm(false);
+    router.push(`/appointment?eventId=${payload.id}`);
   }
 
   return (
     <div>
       <Calendar
         onSelectSlot={handleSelectTime}
+        onSelectEvent={handleSelectEvent}
         localizer={localizer}
-        events={events}
+        backgroundEvents={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}

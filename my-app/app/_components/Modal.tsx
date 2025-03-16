a2d78e56a553820c845d21c3d1b3173a561b7278
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-03-16 22:15:56 +0200

    Create Modal.tsx

---
+++ my-app/app/_components/Modal.tsx
@@ -0,0 +1,18 @@
+export default function Modal({
+  children,
+  onClose,
+}: {
+  children: React.ReactNode;
+  onClose: () => void;
+}) {
+  return (
+    <div className="fixed w-screen h-screen top-0 left-0 bg-gray-400/80 p-2">
+      <div className="translate-middle position-absolute top-50 start-50 h-fit">
+        <button className="absolute right-1 top-1" onClick={onClose}>
+          x
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}

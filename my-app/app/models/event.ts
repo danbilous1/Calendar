2df18aa62aa09824840a548d48e79a7213ed1c6b
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-03-28 23:56:54 +0200

    Create event.ts

---
+++ my-app/app/models/event.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import { EventT, User } from "../type";
+
+const EventSchema = new mongoose.Schema({
+  creatorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  endDate: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    default: 1,
+  },
+});
+
+EventSchema.virtual("appointments", {
+  ref: "Appointment",
+  localField: "_id",
+  foreignField: "eventId",
+});
+
+EventSchema.set("toJSON", {
+  virtuals: true,
+});
+
+EventSchema.set("toObject", {
+  virtuals: true,
+});
+
+export default mongoose.models.Event ||
+  mongoose.model<EventT>("Event", EventSchema);
+
+// type EventT = {
+//   id: string;
+//   creatorId: User["id"];
+//   date?: string;
+//   type: string;
+//   description: string;
+//   schedule?: object;
+//   capacity: number;
+// };

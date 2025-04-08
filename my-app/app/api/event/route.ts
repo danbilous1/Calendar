e4d95c7882bbf0b7d9f9f3770d9d0af3e5ca2a08
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-04-08 22:15:17 +0300

    Update route.ts

--- my-app/app/api/event/route.ts
+++ my-app/app/api/event/route.ts
@@ -2,6 +2,9 @@ import dbConnect from "@/app/lib/dbConnect";
 import { NextRequest } from "next/server";
 import eventModel from "@/app/models/event";
 import "@/app/models/appointment";
+import "@/app/models/user";
+import userModel from "@/app/models/user";
+import { PopulateOptions } from "mongoose";
 
 const mockEvent = {
   _id: "1",
@@ -16,14 +19,25 @@ const mockEvent = {
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const id = searchParams.get("id");
+  const userId = request.headers.get("userId");
 
   await dbConnect();
+  const existingUser = await userModel.findById(userId);
+  let populateObject: PopulateOptions | (string | PopulateOptions)[] = {
+    path: "appointments",
+    match: { userId: userId },
+  };
+  if (existingUser.isAdmin) {
+    populateObject = {
+      path: "appointments",
+    };
+  }
   if (id) {
-    const event = await eventModel.findById(id).populate("appointments");
-    return Response.json(event);
+    const event = await eventModel.findById(id).populate(populateObject);
+    return Response.json({ events: event, isAdmin: existingUser.isAdmin });
   } else {
-    const events = await eventModel.find().populate("appointments");
-    return Response.json(events);
+    const events = await eventModel.find().populate(populateObject);
+    return Response.json({ events: events, isAdmin: existingUser.isAdmin });
   }
 }
 

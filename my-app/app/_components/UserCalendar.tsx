3b8422486123e9b45ac95fe8d9ce9dc7428a8f7f
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-04-16 22:20:11 +0300

    Update UserCalendar.tsx

--- my-app/app/_components/UserCalendar.tsx
+++ my-app/app/_components/UserCalendar.tsx
@@ -10,45 +10,47 @@ import CreateEvent from "@/app/_components/CreateEvent";
 import { EventT, PickCalendarEvent, SelectDateEvent } from "@/app/type";
 import { useRouter } from "next/navigation";
 
-const UserCalendar: FC<{ events: Event[]; appointments: Event[] }> = memo(
-  ({ events, appointments }) => {
-    const router = useRouter();
-    const localizer = useMemo(() => momentLocalizer(moment), []);
-    function handleSelectEvent(payload: unknown) {
-      console.log(2, payload);
-      if (
-        typeof payload == "object" &&
-        payload !== null &&
-        "id" in payload &&
-        "start" in payload &&
-        "end" in payload &&
-        payload.start instanceof Date &&
-        payload.end instanceof Date
-      ) {
-        router.push(
-          `/appointment?eventId=${
-            payload.id
-          }&date=${payload.start.getTime()}&endDate=${payload.end.getTime()}`
-        );
-      }
+const UserCalendar: FC<{ events: Event[]; appointments: Event[] }> = ({
+  events,
+  appointments,
+}) => {
+  console.log("user calendar rendered", events.length, appointments.length);
+  const router = useRouter();
+  const localizer = useMemo(() => momentLocalizer(moment), []);
+  function handleSelectEvent(payload: unknown) {
+    console.log(2, payload);
+    if (
+      typeof payload == "object" &&
+      payload !== null &&
+      "id" in payload &&
+      "start" in payload &&
+      "end" in payload &&
+      payload.start instanceof Date &&
+      payload.end instanceof Date
+    ) {
+      router.push(
+        `/appointment?eventId=${
+          payload.id
+        }&date=${payload.start.getTime()}&endDate=${payload.end.getTime()}`
+      );
     }
-    return (
-      <div>
-        <h1>User View</h1>
-        <Calendar
-          dayLayoutAlgorithm={"no-overlap"}
-          onSelectEvent={handleSelectEvent}
-          localizer={localizer}
-          backgroundEvents={events as unknown as Event[]}
-          events={appointments}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: 500 }}
-          selectable
-        />
-      </div>
-    );
   }
-);
+  return (
+    <div>
+      <h1>User View</h1>
+      <Calendar
+        dayLayoutAlgorithm={"no-overlap"}
+        onSelectEvent={handleSelectEvent}
+        localizer={localizer}
+        backgroundEvents={events as unknown as Event[]}
+        events={appointments}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        selectable
+      />
+    </div>
+  );
+};
 
 export default UserCalendar;

aef175eef353f8dbe24e9d1fd7060295391970bc
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-05-13 22:04:46 +0300

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,60 +1,44 @@
-"use client";
-import { useEffect, useMemo, useState } from "react";
-import AppointmentCart from "./_components/AppointmentCart";
-import Image from "next/image";
-import { Calendar, momentLocalizer, SlotInfo, Event } from "react-big-calendar";
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import CreateEvent from "./_components/CreateEvent";
-import { EventT, PickCalendarEvent, SelectDateEvent } from "./type";
-import { useRouter } from "next/navigation";
-import appointment from "./models/appointment";
-import { Result } from "postcss";
+"use server";
+import { Event } from "react-big-calendar";
+import { Appointment, EventT } from "./type";
 import AdminCalendar from "./_components/AdminCalendar";
-import UserCalendar from "./_components/UserCalendar";
+import { getevents } from "@/app/actions/getevents";
+import { headers } from "next/headers";
 
-type ResultT = { events: EventT[]; isAdmin: boolean };
-
-const MyCalendar = () => {
-  const [events, setEvents] = useState<Event[]>([]);
-  const [isAdmin, setIsAdmin] = useState<boolean>(false);
-  const [appointments, setAppointments] = useState<Event[]>([]);
-
-  useEffect(() => {
-    fetch("/api/event")
-      .then((res) => res.json())
-      .then((result: ResultT) => {
-        setIsAdmin(result.isAdmin);
-        const bigCalendarEvent = result.events.map((event) => {
-          if (event?.appointments?.length) {
-            setAppointments((prev) =>
-              prev.concat(
-                event?.appointments?.map((appointment) => ({
-                  id: appointment._id,
-                  event_id: event._id,
-                  title:
-                    "appointment " + event.description + appointment.status,
-                  start: new Date(appointment.date || event.date),
-                  end: new Date(appointment.endDate || event.endDate),
-                  type: "appointment",
-                })) || []
-              )
-            );
-          }
-          return {
-            id: event._id,
-            title: "event " + event.description,
-            start: new Date(event.date),
-            end: new Date(event.endDate),
-            type: "event",
-          };
-        });
-
-        setEvents(bigCalendarEvent);
+const MyCalendar = async () => {
+  const headersObject = await headers();
+  const userId = headersObject.get("userId");
+  const { events, isAdmin, appointments } = await getevents(userId).then(
+    ([eventsList, isAdmin]) => {
+      let appointments: Event[] = [];
+      const events = eventsList.map((event) => {
+        if (event?.appointments?.length) {
+          appointments.push(
+            ...(event?.appointments?.map((appointment) => ({
+              id: appointment._id,
+              event_id: event._id,
+              title: "appointment " + event.description + appointment.status,
+              start: new Date(appointment.date || event.date),
+              end: new Date(appointment.endDate || event.endDate),
+              type: "appointment",
+            })) || [])
+          );
+        }
+        return {
+          id: event._id,
+          title: "event " + event.description,
+          start: new Date(event.date),
+          end: new Date(event.endDate),
+          type: "event",
+        };
       });
-  }, []);
-
-  console.log(events, appointments);
+      return {
+        events,
+        isAdmin: isAdmin,
+        appointments,
+      };
+    }
+  );
 
   return (
     <div>
@@ -67,5 +51,3 @@ const MyCalendar = () => {
   );
 };
 export default MyCalendar;
-
-// MAKE LIST OF FEATURES, THAT WE DIDNT ADD YET

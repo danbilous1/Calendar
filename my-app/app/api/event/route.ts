51c1c5fea0fca33c06d5979dbfd1269f188d65dc
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-05-17 23:33:03 +0300

    Update route.ts

--- my-app/app/api/event/route.ts
+++ my-app/app/api/event/route.ts
@@ -5,6 +5,8 @@ import "@/app/models/appointment";
 import "@/app/models/user";
 import userModel from "@/app/models/user";
 import { PopulateOptions } from "mongoose";
+import { getevents } from "@/app/actions/getevents";
+import { revalidatePath } from "next/cache";
 
 const mockEvent = {
   _id: "1",
@@ -17,28 +19,9 @@ const mockEvent = {
 };
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const id = searchParams.get("id");
   const userId = request.headers.get("userId");
-
-  await dbConnect();
-  const existingUser = await userModel.findById(userId);
-  let populateObject: PopulateOptions | (string | PopulateOptions)[] = {
-    path: "appointments",
-    match: { userId: userId },
-  };
-  if (existingUser.isAdmin) {
-    populateObject = {
-      path: "appointments",
-    };
-  }
-  if (id) {
-    const event = await eventModel.findById(id).populate(populateObject);
-    return Response.json({ events: event, isAdmin: existingUser.isAdmin });
-  } else {
-    const events = await eventModel.find().populate(populateObject);
-    return Response.json({ events: events, isAdmin: existingUser.isAdmin });
-  }
+  const [events, isAdmin] = await getevents(userId);
+  return Response.json({ events: events, isAdmin: isAdmin });
 }
 
 export async function POST(request: Request) {
@@ -47,8 +30,44 @@ export async function POST(request: Request) {
   const existingUser = await userModel.findById(userId);
   try {
     const body = await request.json();
+    body.creatorId = userId;
     console.log(body);
     if (existingUser.isAdmin) {
+      const newEvent = await eventModel.create(body);
+      return Response.json(newEvent);
+    }
+
+    return Response.json({ message: "You are not allowed." }, { status: 403 });
+  } catch (error) {
+    return Response.json(error);
+  }
+}
+
+export async function PUT(request: Request) {
+  await dbConnect();
+  const userId = request.headers.get("userId");
+  const existingUser = await userModel.findById(userId);
+  try {
+    const body = await request.json();
+    console.log(body);
+    if (existingUser.isAdmin) {
+      const findEvent = await eventModel.findById(body._id);
+      if (findEvent) {
+        findEvent.description = body?.description ?? findEvent.description;
+        findEvent.date = body?.date ?? findEvent.date;
+        findEvent.endDate = body?.endDate ?? findEvent.endDate;
+        findEvent.type = body?.type ?? findEvent.type;
+        findEvent.capacity = body?.capacity ?? findEvent.capacity;
+
+        await findEvent.save();
+        revalidatePath("/");
+        return Response.json(
+          { message: "Event updated successful" },
+          { status: 201 }
+        );
+      } else {
+        return Response.json({ message: "Event not found." }, { status: 404 });
+      }
       const newEvent = await eventModel.create(body);
       return Response.json(newEvent);
     }

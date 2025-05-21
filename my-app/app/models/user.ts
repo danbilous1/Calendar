a2af531583124e26b5d922899a05b5f12c24448d
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-05-21 23:17:53 +0300

    Update user.ts

--- my-app/app/models/user.ts
+++ my-app/app/models/user.ts
@@ -1,24 +1,33 @@
 import mongoose from "mongoose";
-import { User } from "../type";
+import { User } from "@/app/type";
 
-const UserSchema = new mongoose.Schema<User>({
-  isAdmin: {
-    type: Boolean,
-    required: false,
-    default: false,
+const UserSchema = new mongoose.Schema<User>(
+  {
+    isAdmin: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
-export default mongoose.models.User || mongoose.model<User>("User", UserSchema);
+export default mongoose?.models?.User ||
+  mongoose.model<User>("User", UserSchema);

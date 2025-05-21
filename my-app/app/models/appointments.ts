97ce8833f8118e6d0d41070b9bf60271a084102e
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-05-21 23:17:40 +0300

    Update appointments.ts

--- my-app/app/models/appointments.ts
+++ my-app/app/models/appointments.ts
@@ -1,31 +1,43 @@
 import mongoose from "mongoose";
 import { Appointment } from "../type";
 
-const AppointmentSchema = new mongoose.Schema({
-  eventId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Event",
-    required: true,
+const AppointmentSchema = new mongoose.Schema(
+  {
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: String,
+      required: false,
+    },
+    endDate: {
+      type: String,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["scheduled", "confirmed", "paid", "canceled", "commited"],
+    },
+    notes: {
+      type: String,
+    },
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  date: {
-    type: String,
-    required: false,
-  },
-  endDate: {
-    type: String,
-    required: false,
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["scheduled", "confirmed", "paid", "canceled", "commited"],
-  },
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
-export default mongoose.models.Appointment ||
+export default mongoose?.models?.Appointment ||
   mongoose.model<Appointment>("Appointment", AppointmentSchema);
+
